34ceabc63c318d338a11b916808770b21f126dfa
evinsdesign <kelvinsaputra024@gmail.com>
2024-05-03 15:20:29 +0800

    feat: edit data fix

--- apps/AdminController.php
+++ apps/AdminController.php
@@ -1,12 +1,17 @@
 <?php
-require_once 'AdminModel.php';
-// query memunculkan table 
-$admins = query("SELECT * FROM `admin`");
-
+require_once '../config/config.php';
 // query mengecek fungsi tambahdata berhasil atau tidak
 if (isset($_POST['TambahData'])) {
+
+    $nama = htmlspecialchars($_POST['nama']);
+    $username = htmlspecialchars($_POST['username']);
+    $password = htmlspecialchars($_POST['password']);
+    $gambar = htmlspecialchars($_POST['image_upload']);
+
+    $query = "INSERT INTO `admin` (nama, username, password, gambar) VALUES ('$nama', '$username', '$password', '$gambar')";
+    $simpan = mysqli_query($koneksi, $query);
     // memunculkan fungsi tambah
-    if (tambah($_POST) > 0) {
+    if ($simpan) {
         echo "<script>
         alert('Data Berhasil Ditambahkan')
         document.location.href='http://localhost/pencarian_pintar_lontar/pages/admin/TableDataAdmin.php'
@@ -18,17 +23,52 @@ if (isset($_POST['TambahData'])) {
     }
 }
 
-// query mengecek fungsi tambahdata berhasil atau tidak
-if (isset($_POST['EditData'])) {
+// query mengecek fungsi Hapus berhasil atau tidak
+if (isset($_POST['HapusData'])) {
+
+    $id = $_POST['id_admin'];
+    $query = "DELETE FROM `admin` WHERE id = $id";
+    $delete = mysqli_query($koneksi, $query);
     // memunculkan fungsi tambah
-    if (edit($_POST) > 0) {
+    if ($delete) {
         echo "<script>
-        alert('Data Berhasil diupdate')
+        alert('Data Berhasil didelete')
         document.location.href='http://localhost/pencarian_pintar_lontar/pages/admin/TableDataAdmin.php'
     </script>";
     } else {
         echo "<script>
-        alert('Data gagal diupdate')
+        alert('Data gagal didelete')
+    </script>";
+    }
+}
+
+// query mengecek fungsi tombol EditData berhasil atau tidak
+if (isset($_POST['EditData'])) {
+    // mengambil data dengan method post
+    $id = $_POST['id_admin'];
+    $nama = htmlspecialchars($_POST['nama']);
+    $username = htmlspecialchars($_POST['username']);
+    $password = htmlspecialchars($_POST['password']);
+    $gambar = htmlspecialchars($_POST['image_upload']);
+
+    $query = "UPDATE `admin` SET
+                nama = '$nama',
+                username = '$username',
+                password = '$password',
+                gambar = '$gambar'
+                WHERE id = '$id'
+                 ";
+    $update = mysqli_query($koneksi, $query);
+    // memunculkan fungsi tambah
+    if ($update) {
+        echo "<script>
+        alert('Data Berhasil Diubah')
+        document.location.href='http://localhost/pencarian_pintar_lontar/pages/admin/TableDataAdmin.php'
+    </script>";
+    } else {
+        echo "<script>
+        alert('Data gagal Diubah')
+        document.location.href='http://localhost/pencarian_pintar_lontar/pages/admin/TableDataAdmin.php'
     </script>";
     }
 }
