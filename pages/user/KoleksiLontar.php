bbbd86a22718a4bb324581ad0c2ceb798157aca5
evinsdesign <kelvinsaputra024@gmail.com>
2024-06-23 21:30:14 +0800

    fix:responsive search engine

--- pages/user/KoleksiLontar.php
+++ pages/user/KoleksiLontar.php
@@ -16,7 +16,7 @@ require_once '../../apps/ViewLontar.php';
 <body class="font-montserrat overflow-x-hidden">
     <header>
         <!-- navbar -->
-        <nav class="bg-darkBlue flex justify-between items-center px-7 py-5">
+        <nav class="bg-darkBlue flex justify-between items-center px-7 py-3">
             <!-- logo -->
             <div class="flex items-center">
                 <svg class="h-9 w-auto" width="236" height="70" viewBox="0 0 236 70" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -41,30 +41,30 @@ require_once '../../apps/ViewLontar.php';
             </div>
         </nav>
         <!-- end navbar -->
-
-        <!-- search component -->
-        <section class="flex flex-col justify-center items-center absolute z-40 xxsm:-translate-x-1/2 xxsm:-translate-y-60 xsm:-translate-y-50 base:-translate-y-[14rem] sm:-translate-y-48 md:-translate-y-36 lg:-translate-y-40 xxsm:top-1/2 xxsm:left-1/2">
-            <h1 class="text-white xl:mb-3 font-medium xxsm:text-sm xsm:text-base sm:text-xl md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl xxsm:mb-0">
-                Mulai Eksplorasi
-                <span class="font-montsBold text-orangePastel">Pencarian Lontar</span>
-            </h1>
-            <form action="http://localhost/pencarian_pintar_lontar/pages/user/KoleksiLontar.php" method="post" class="">
-                <div class="flex m-0 drop-shadow-[1px_4px_43.4px_rgba(0,0,0,0.50)]">
-                    <div class="relative flex items-center text-lightSecondary focus-within:text-mediumBlue">
-                        <i class="fa-solid fa-magnifying-glass absolute xxsm:text-base lg:text-xl 2xl:text-2xl xxsm:mt-2 xxsm:ml-3 mt-5 ml-5 text-mediumBlue" name="search"></i>
-                        <input type="text" placeholder="Cari Lontar" name="keyword" autocomplete="off" aria-label="Cari Lontar" class="mt-5 xxsm:mt-2 xxsm:pl-12 px-16 placeholder-darkSecondary focus:placeholder-lightBlue xxsm:w-[200px] xxsm:h-9 sm:text-xl xsm:w-[250px] xsm:h-9 sm:w-[350px] sm:h-10 md:text-xl lg:text-2xl md:w-[300px] md:h-[50px] lg:w-[400px] lg:h-[55px] xl:w-[500px] xl:h-[60px] 2xl:w-[700px] 2xl:h-[70px] rounded-s-[15px] border-none ring-2 ring-mediumBlue focus:ring-orangePastel focus:ring-2" />
-                    </div>
-                    <button type="submit" name="btn_keyword" class="bg-mediumBlue xxsm:mt-2 xxsm:text-base sm:px-3 xsm:text-lg xxsm:px-3 xsm:px-3 sm:text-xl md:text-2xl xl:text-3xl md:w-[100px] md:h-[50px] lg:w-[110px] lg:h-[55px] xl:w-[120px] xl:h-[60px] 2xl:w-[120px] 2xl:h-[70px] mt-5 text-orangePastel text-[24px] rounded-r-[15px] ring-2 ring-mediumBlue">
-                        Cari
-                    </button>
-                </div>
-            </form>
-        </section>
-        <!-- end search component -->
         <!-- carousel image -->
         <div id="default-carousel" class="relative w-full mb-5 " data-carousel="slide">
+            <!-- search component -->
+            <section class="flex flex-col justify-center items-center absolute z-40 inset-0">
+                <h1 class=" text-white xl:mb-3 font-medium xxsm:text-sm xsm:text-base sm:text-xl md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl xxsm:mb-0">
+                    Mulai Eksplorasi
+                    <span class="font-montsBold text-orangePastel">Pencarian Lontar</span>
+                </h1>
+                <form action="http://localhost/pencarian_pintar_lontar/pages/user/KoleksiLontar.php" method="post" class="">
+                    <div class="flex m-0 drop-shadow-[1px_4px_43.4px_rgba(0,0,0,0.50)]">
+                        <div class="relative flex items-center text-lightSecondary focus-within:text-mediumBlue">
+                            <i class="fa-solid fa-magnifying-glass absolute xxsm:text-base lg:text-xl 2xl:text-2xl xxsm:mt-2 xxsm:ml-3 mt-5 ml-5 text-mediumBlue" name="search"></i>
+                            <input type="text" placeholder="Cari Lontar" name="keyword" autocomplete="off" aria-label="Cari Lontar" class="mt-5 xxsm:mt-2 xxsm:pl-12 px-16 placeholder-darkSecondary focus:placeholder-lightBlue xxsm:w-[200px] xxsm:h-9 sm:text-xl xsm:w-[250px] xsm:h-9 sm:w-[350px] sm:h-10 md:text-xl lg:text-2xl md:w-[300px] md:h-[50px] lg:w-[400px] lg:h-[55px] xl:w-[500px] xl:h-[60px] 2xl:w-[700px] 2xl:h-[70px] rounded-s-[15px] border-none ring-2 ring-mediumBlue focus:ring-orangePastel focus:ring-2" />
+                        </div>
+                        <button type="submit" name="btn_keyword" class="bg-mediumBlue xxsm:mt-2 xxsm:text-base sm:px-3 xsm:text-lg xxsm:px-3 xsm:px-3 sm:text-xl md:text-2xl xl:text-3xl md:w-[100px] md:h-[50px] lg:w-[110px] lg:h-[55px] xl:w-[120px] xl:h-[60px] 2xl:w-[120px] 2xl:h-[70px] mt-5 text-orangePastel text-[24px] rounded-r-[15px] ring-2 ring-mediumBlue">
+                            Cari
+                        </button>
+                    </div>
+                </form>
+            </section>
+            <!-- end search component -->
+
             <!-- Carousel wrapper -->
-            <div class="relative xxsm:h-56 base:h-60 sm:h-72 md:h-[25rem] lg:h-96 overflow-hidden xl:h-[30rem] 2xl:h-[35rem]">
+            <div class="relative xxsm:h-52 base:h-60 sm:h-72 md:h-[25rem] lg:h-96 overflow-hidden xl:h-[30rem] 2xl:h-[35rem]">
                 <!-- Item 1 -->
                 <div class="hidden duration-700 ease-in-out" data-carousel-item>
                     <img src="../../public/assets/bg-header(1).jpg" class="absolute block w-full lg:-translate-x-1/2 lg:-translate-y-1/2 lg:top-1/2 lg:left-1/2 bg-cover xxsm:h-[210px] xsm:h-[240px] base:h-[290px] sm:h-[300px] md:h-[500px] lg:h-[500px] xl:h-[600px] 2xl:h-[800px]" alt="bg-header1" />
@@ -106,8 +106,8 @@ require_once '../../apps/ViewLontar.php';
     </header>
     <main>
         <div class="flex justify-center items-center flex-col">
-            <h1 class="font-montsBold text-darkBlue text-2xl">Koleksi Naskah Lontar Bali</h1>
-            <p class="text-darkSecondary">Terdapat
+            <h1 class="xxsm:text-base font-montsBold text-darkBlue lg:text-2xl">Koleksi Naskah Lontar Bali</h1>
+            <p class="xxsm:text-sm xxsm:w-72 lg:text-base text-center text-darkSecondary">Terdapat
                 <span class="text-mediumBlue font-montsMedium">
                     <!-- menghitung banyak data lontar -->
                     <?php
@@ -123,8 +123,8 @@ require_once '../../apps/ViewLontar.php';
         <?php
 
         ?>
-        <div class="w-[1550px]">
-            <div class="flex justify-end">
+        <div class="xxsm:mt-5 2xl:w-[1550px]">
+            <div class="flex justify-center 2xl:flex 2xl:justify-end">
                 <form action="" method="post" class="flex">
                     <select name="klasifikasi_lontar" id="dropdownDelayButton" data-dropdown-toggle="dropdownDelay" data-dropdown-delay="500" data-dropdown-trigger="hover" class="text-white bg-darkBlue hover:bg-mediumBlue focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-s-[15px] text-sm px-5 py-3 text-center inline-flex items-center gap-2 dark:bg-darkBlue dark:hover:mediumBlue dark:focus:ring-blue-800 border-r-2 border-darkBlue appearance-none">
                         <option>Klasifikasi Lontar</option>
@@ -460,13 +460,13 @@ require_once '../../apps/ViewLontar.php';
                         <div class="flex xxsm:flex-col  md:flex-row items-center rounded-lg xxsm:w-[250px] xsm:w-[280px] base:w-[360px] sm:w-[560px] md:w-[650px] lg:w-[800px] xl:w-[900px] 2xl:w-[700px] max-h-full shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] xxsm:flex">
                             <div class="flex flex-col justify-between p-4 leading-normal xxsm:order-2 md:order-none md:text-sm lg:text-base">
                                 <input type="hidden" name="title" value="<?= $data->title ?>">
-                                <h2 class="xl:card-title text-darkBlue font-montsBold xxsm:text-lg base:text-xl xl:text-lg">
+                                <h2 class="xl:card-title text-darkBlue font-montsBold xxsm:text-base base:text-xl xl:text-lg">
                                     <?= $data->title ?>
                                 </h2>
-                                <h2 class="font-montsMedium text-mediumBlue xl:text-sm">
+                                <h2 class="font-montsMedium xxsm:text-sm text-mediumBlue xl:text-sm">
                                     <?= $data->author ?> | <?= $data->year ?> | <?= $data->area ?> | <?= $data->regency ?>
                                 </h2>
-                                <p class="font-montserrat text-justify xl:text-sm">
+                                <p class="font-montserrat text-justify xxsm:text-sm xl:text-sm">
                                     Detail Deskripsi Lengkap Lontar, judul lontar : <?= $data->title ?>, tipe bahan:
                                     <?= $data->type ?>, subjek: <?= $data->subject ?>, klasifikasi: <?= $data->classification ?>,
                                     bahasa: <?= $data->language ?> <a href="/pencarian_pintar_lontar/pages/user/DetailLontar.php?id=<?= $data->title ?>" class="text-blue-600 underline underline-offset-2 font-montsMedium">Selengkapnya</a>
--- pages/user/TentangKami.php
+++ pages/user/TentangKami.php
@@ -12,7 +12,7 @@
 <body class="font-montserrat">
     <header>
         <!-- navbar -->
-        <nav class="bg-darkBlue flex justify-between items-center px-7 py-5">
+        <nav class="bg-darkBlue flex justify-between items-center px-7 py-3">
             <!-- logo -->
             <div class="flex items-center">
                 <svg class="h-9 w-auto" width="236" height="70" viewBox="0 0 236 70" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -37,28 +37,27 @@
             </div>
         </nav>
         <!-- end navbar -->
-
-        <!-- search component -->
-        <section class="flex flex-col justify-center items-center absolute z-40 xxsm:-translate-x-1/2 xxsm:-translate-y-60 xsm:-translate-y-50 base:-translate-y-[14rem] sm:-translate-y-48 md:-translate-y-36 lg:-translate-y-40 xxsm:top-1/2 xxsm:left-1/2">
-            <h1 class="text-white xl:mb-3 font-medium xxsm:text-sm xsm:text-base sm:text-xl md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl xxsm:mb-0">
-                Mulai Eksplorasi
-                <span class="font-montsBold text-orangePastel">Pencarian Lontar</span>
-            </h1>
-            <form action="KoleksiLontar.php" method="post" class="">
-                <div class="flex m-0 drop-shadow-[1px_4px_43.4px_rgba(0,0,0,0.50)]">
-                    <div class="relative flex items-center text-lightSecondary focus-within:text-mediumBlue">
-                        <i class="fa-solid fa-magnifying-glass absolute xxsm:text-base lg:text-xl 2xl:text-2xl xxsm:mt-2 xxsm:ml-3 mt-5 ml-5 text-mediumBlue" name="search"></i>
-                        <input type="text" placeholder="Cari Lontar" name="keyword" autocomplete="off" aria-label="Cari Lontar" class="mt-5 xxsm:mt-2 xxsm:pl-12 px-16 placeholder-darkSecondary focus:placeholder-lightBlue xxsm:w-[200px] xxsm:h-9 sm:text-xl xsm:w-[250px] xsm:h-9 sm:w-[350px] sm:h-10 md:text-xl lg:text-2xl md:w-[300px] md:h-[50px] lg:w-[400px] lg:h-[55px] xl:w-[500px] xl:h-[60px] 2xl:w-[700px] 2xl:h-[70px] rounded-s-[15px] border-none ring-2 ring-mediumBlue focus:ring-orangePastel focus:ring-2" />
-                    </div>
-                    <button type="submit" name="btn_keyword" class="bg-mediumBlue xxsm:mt-2 xxsm:text-base sm:px-3 xsm:text-lg xxsm:px-3 xsm:px-3 sm:text-xl md:text-2xl xl:text-3xl md:w-[100px] md:h-[50px] lg:w-[110px] lg:h-[55px] xl:w-[120px] xl:h-[60px] 2xl:w-[120px] 2xl:h-[70px] mt-5 text-orangePastel text-[24px] rounded-r-[15px] ring-2 ring-mediumBlue">
-                        Cari
-                    </button>
-                </div>
-            </form>
-        </section>
-        <!-- end search component -->
         <!-- carousel image -->
         <div id="default-carousel" class="relative w-full" data-carousel="slide">
+            <!-- search component -->
+            <section class="flex flex-col justify-center items-center absolute z-40 inset-0">
+                <h1 class="text-white xl:mb-3 font-medium xxsm:text-sm xsm:text-base sm:text-xl md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl xxsm:mb-0">
+                    Mulai Eksplorasi
+                    <span class="font-montsBold text-orangePastel">Pencarian Lontar</span>
+                </h1>
+                <form action="KoleksiLontar.php" method="post" class="">
+                    <div class="flex m-0 drop-shadow-[1px_4px_43.4px_rgba(0,0,0,0.50)]">
+                        <div class="relative flex items-center text-lightSecondary focus-within:text-mediumBlue">
+                            <i class="fa-solid fa-magnifying-glass absolute xxsm:text-base lg:text-xl 2xl:text-2xl xxsm:mt-2 xxsm:ml-3 mt-5 ml-5 text-mediumBlue" name="search"></i>
+                            <input type="text" placeholder="Cari Lontar" name="keyword" autocomplete="off" aria-label="Cari Lontar" class="mt-5 xxsm:mt-2 xxsm:pl-12 px-16 placeholder-darkSecondary focus:placeholder-lightBlue xxsm:w-[200px] xxsm:h-9 sm:text-xl xsm:w-[250px] xsm:h-9 sm:w-[350px] sm:h-10 md:text-xl lg:text-2xl md:w-[300px] md:h-[50px] lg:w-[400px] lg:h-[55px] xl:w-[500px] xl:h-[60px] 2xl:w-[700px] 2xl:h-[70px] rounded-s-[15px] border-none ring-2 ring-mediumBlue focus:ring-orangePastel focus:ring-2" />
+                        </div>
+                        <button type="submit" name="btn_keyword" class="bg-mediumBlue xxsm:mt-2 xxsm:text-base sm:px-3 xsm:text-lg xxsm:px-3 xsm:px-3 sm:text-xl md:text-2xl xl:text-3xl md:w-[100px] md:h-[50px] lg:w-[110px] lg:h-[55px] xl:w-[120px] xl:h-[60px] 2xl:w-[120px] 2xl:h-[70px] mt-5 text-orangePastel text-[24px] rounded-r-[15px] ring-2 ring-mediumBlue">
+                            Cari
+                        </button>
+                    </div>
+                </form>
+            </section>
+            <!-- end search component -->
             <!-- Carousel wrapper -->
             <div class="relative xxsm:h-52 base:h-60 sm:h-72 md:h-[25rem] lg:h-96 overflow-hidden xl:h-[30rem] 2xl:h-[35rem]">
                 <!-- Item 1 -->
