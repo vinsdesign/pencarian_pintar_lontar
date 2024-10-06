06158ed202c1b05433e5da4b512bd141fee1969c
evinsdesign <kelvinsaputra024@gmail.com>
2024-10-06 20:17:32 +0800

    fix: no image and disable debug search

--- pages/user/KoleksiLontar.php
+++ pages/user/KoleksiLontar.php
@@ -148,7 +148,7 @@ include_once '../../config/URLconfig.php';
                     }
 
                     $filter_query = implode(" || ", $filters);
-                    var_dump($filters);
+                    // var_dump($filters);
                     $query = "
                     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
                     PREFIX lontar: <http://www.semanticweb.org/sarasvananda/ontologies/2023/5/untitled-ontology-12#>
